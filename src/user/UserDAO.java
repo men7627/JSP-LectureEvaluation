dc327cf2684b44045df1c1247facd5c5e05fe5e6
men7627 <men1219@hanmail.net>
2019-05-21 01:39:19 +0900

    Add : setEmailChecked

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -135,4 +135,43 @@ public class UserDAO {
 		} 
 		return false; // 데이터베이스 오류
 	}
+	
+	public Boolean setUserEmailChecked(UserDTO user) {
+		String SQL = "UPDATE USER SET userEmailChecked = true WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} 
+		return false; // 데이터베이스 오류
+	}
 }
