3f58817eb13875ffc701f3274c21d61eca6ea29b
men7627 <men1219@hanmail.net>
2019-05-23 01:46:19 +0900

    Add : EvaluationDTO

---
+++ src/evaluation/EvaluationDTO.java
@@ -0,0 +1,120 @@
+package evaluation;
+
+public class EvaluationDTO {
+	int evaluationID;
+	String userID;
+	String lectureName;
+	String professerName;
+	int lectureYear;
+	String semesterDivide;
+	String lectureDivide;
+	String evaluationTitle;
+	String totalScore;
+	String creditScore;
+	String comfortableScore;
+	String lectureScore;
+	int likeCount;
+	
+	public EvaluationDTO() {
+		
+	}
+	
+	public EvaluationDTO(int evaluationID, String userID, String lectureName, String professerName, int lectureYear,
+			String semesterDivide, String lectureDivide, String evaluationTitle, String totalScore, String creditScore,
+			String comfortableScore, String lectureScore, int likeCount) {
+		super();
+		this.evaluationID = evaluationID;
+		this.userID = userID;
+		this.lectureName = lectureName;
+		this.professerName = professerName;
+		this.lectureYear = lectureYear;
+		this.semesterDivide = semesterDivide;
+		this.lectureDivide = lectureDivide;
+		this.evaluationTitle = evaluationTitle;
+		this.totalScore = totalScore;
+		this.creditScore = creditScore;
+		this.comfortableScore = comfortableScore;
+		this.lectureScore = lectureScore;
+		this.likeCount = likeCount;
+	}
+	public int getEvaluationID() {
+		return evaluationID;
+	}
+	public void setEvaluationID(int evaluationID) {
+		this.evaluationID = evaluationID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getLectureName() {
+		return lectureName;
+	}
+	public void setLectureName(String lectureName) {
+		this.lectureName = lectureName;
+	}
+	public String getProfesserName() {
+		return professerName;
+	}
+	public void setProfesserName(String professerName) {
+		this.professerName = professerName;
+	}
+	public int getLectureYear() {
+		return lectureYear;
+	}
+	public void setLectureYear(int lectureYear) {
+		this.lectureYear = lectureYear;
+	}
+	public String getSemesterDivide() {
+		return semesterDivide;
+	}
+	public void setSemesterDivide(String semesterDivide) {
+		this.semesterDivide = semesterDivide;
+	}
+	public String getLectureDivide() {
+		return lectureDivide;
+	}
+	public void setLectureDivide(String lectureDivide) {
+		this.lectureDivide = lectureDivide;
+	}
+	public String getEvaluationTitle() {
+		return evaluationTitle;
+	}
+	public void setEvaluationTitle(String evaluationTitle) {
+		this.evaluationTitle = evaluationTitle;
+	}
+	public String getTotalScore() {
+		return totalScore;
+	}
+	public void setTotalScore(String totalScore) {
+		this.totalScore = totalScore;
+	}
+	public String getCreditScore() {
+		return creditScore;
+	}
+	public void setCreditScore(String creditScore) {
+		this.creditScore = creditScore;
+	}
+	public String getComfortableScore() {
+		return comfortableScore;
+	}
+	public void setComfortableScore(String comfortableScore) {
+		this.comfortableScore = comfortableScore;
+	}
+	public String getLectureScore() {
+		return lectureScore;
+	}
+	public void setLectureScore(String lectureScore) {
+		this.lectureScore = lectureScore;
+	}
+	public int getLikeCount() {
+		return likeCount;
+	}
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+	
+	
+}
