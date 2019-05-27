480ab2c6c1b742b369fce0165838ca9702c75331
men7627 <men1219@hanmail.net>
2019-05-28 02:48:52 +0900

    Add : like,delte to EvaluationDAO and LikeyDAO, LikeyDTO

--- src/evaluation/EvaluationDAO.java
+++ src/evaluation/EvaluationDAO.java
@@ -57,8 +57,9 @@ public class EvaluationDAO {
 		}
 		return -1; // 데이터베이스 오류
 	}
-	public ArrayList<EvaluationDTO> getList (String lectureDivide, String searchType, String search, int pageNumber){
-		if(lectureDivide.equals("전체")) {
+
+	public ArrayList<EvaluationDTO> getList(String lectureDivide, String searchType, String search, int pageNumber) {
+		if (lectureDivide.equals("전체")) {
 			lectureDivide = "";
 		}
 		ArrayList<EvaluationDTO> evaluationList = null;
@@ -67,12 +68,12 @@ public class EvaluationDAO {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
-			if(searchType.equals("최신순")) {
-				SQL = "SELECT * FROM EVALUATION WHERE lectureDivide LIKE ? AND CONCAT(lectureName, professorName, evaluationTitle, evaluationContent) LIKE" +
-						"? ORDER BY evaluationID DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
+			if (searchType.equals("최신순")) {
+				SQL = "SELECT * FROM EVALUATION WHERE lectureDivide LIKE ? AND CONCAT(lectureName, professorName, evaluationTitle, evaluationContent) LIKE"
+						+ "? ORDER BY evaluationID DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
 			} else if (searchType.equals("추천순")) {
-				SQL = "SELECT * FROM EVALUATION WHERE lectureDivide LIKE ? AND CONCAT(lectureName, professorName, evaluationTitle, evaluationContent) LIKE" +
-						"? ORDER BY likeCount DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
+				SQL = "SELECT * FROM EVALUATION WHERE lectureDivide LIKE ? AND CONCAT(lectureName, professorName, evaluationTitle, evaluationContent) LIKE"
+						+ "? ORDER BY likeCount DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
 			}
 			conn = DatabaseUtil.getConnection();
 			pstmt = conn.prepareStatement(SQL);
@@ -81,22 +82,10 @@ public class EvaluationDAO {
 			rs = pstmt.executeQuery();
 			evaluationList = new ArrayList<EvaluationDTO>();
 			while (rs.next()) {
-				EvaluationDTO evaluationDTO = new EvaluationDTO(
-						rs.getInt(1),
-						rs.getString(2),
-						rs.getString(3),
-						rs.getString(4),
-						rs.getInt(5),
-						rs.getString(6),
-						rs.getString(7),
-						rs.getString(8),
-						rs.getString(9),
-						rs.getString(10),
-						rs.getString(11),
-						rs.getString(12),
-						rs.getString(13),
-						rs.getInt(14)
-				);
+				EvaluationDTO evaluationDTO = new EvaluationDTO(rs.getInt(1), rs.getString(2), rs.getString(3),
+						rs.getString(4), rs.getInt(5), rs.getString(6), rs.getString(7), rs.getString(8),
+						rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13),
+						rs.getInt(14));
 				evaluationList.add(evaluationDTO);
 			}
 		} catch (Exception e) {
@@ -125,5 +114,122 @@ public class EvaluationDAO {
 			}
 		}
 		return evaluationList; // 데이터베이스 오류
+	}
+
+	public int like(String evaluationID) {
+		String SQL = "UPDATE EVALUATION SET likeCount = likeCount + 1 WHERE evaluationID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, Integer.parseInt(evaluationID));
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} 
+		return -1; // 데이터베이스 오류
+	}
+	
+	public int delete(String evaluationID) {
+		String SQL = "DELETE FROM EVALUATION WHERE evaluationID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, Integer.parseInt(evaluationID));
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} 
+		return -1; // 데이터베이스 오류
+	}
+	
+	public String getUserID(String evaluationID) {
+		String SQL = "SELECT userID FROM EVALUATION WHERE evaluationID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, Integer.parseInt(evaluationID));
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} 
+		return null; // 아이디 존재x
 	}
 }
---
+++ src/likey/LikeyDAO.java
@@ -0,0 +1,51 @@
+package likey;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import util.DatabaseUtil;
+
+public class LikeyDAO {
+	public int like(String userID, String evaluation, String userIP) {
+		String SQL = "INSERT INTO USER VALUES (?,?,?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.setString(2, evaluation);
+			pstmt.setString(3, userIP);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} 
+		return -1; // 추천 중복 오류
+	}
+
+}
